85dfc7898465cac603ba987445424c20e59d858c
tyange <usun16@gmail.com>
2022-07-18 19:39:17 +0900

    header 꾸미기

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,51 @@
 import * as React from "react";
+import styled from "styled-components";
+import { Email } from "@styled-icons/entypo/Email";
+import { QuestionMark } from "@styled-icons/open-iconic/QuestionMark";
+
+import tyangeLogo from "../images/tyange-logo.svg";
+
+const StyledHeader = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+`;
+
+const HeaderWrapper = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 5rem;
+`;
+
+const Logo = styled.div``;
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <span>logo</span>
-      </div>
-      <div>
-        <span>mail</span>
-        <span>credits</span>
-      </div>
-    </header>
+    <StyledHeader>
+      <HeaderWrapper>
+        <div>
+          <span>
+            <img src={tyangeLogo} />
+          </span>
+        </div>
+        <Menu>
+          <span>
+            <Email size="20" />
+          </span>
+          <span>
+            <QuestionMark size="18" />
+          </span>
+        </Menu>
+      </HeaderWrapper>
+    </StyledHeader>
   );
 };
 
