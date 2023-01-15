e47358e6c4c73ca50b899e52c7edb7370bd2bd72
tyange <usun16@gmail.com>
2023-01-15 16:27:26 +0900

    meta tag og property 추가

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -27,6 +27,13 @@ export const SEO = ({ title }: Props) => {
     <Helmet>
       <title>{`${site.siteMetadata.title}${title && `  |  ${title}`}`}</title>
       <meta name="description" content={site.siteMetadata.description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://tyange.gatsbyjs.io/" />
+      <meta property="og:title" content={site.siteMetadata.title} />
+      <meta property="og:image" content={tyangeLogo} />
+      <meta property="og:description" content={site.siteMetadata.description} />
+      <meta property="og:site_name" content="tyange blog" />
+      <meta property="og:locale" content="ko_KR" />
       <link rel="icon" href={tyangeLogo} />
     </Helmet>
   );
